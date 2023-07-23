c9e712ec4d0e197475583921b9d05373bd5b12be
Mahesh <58337761+MaheshMadushan@users.noreply.github.com>
2023-07-23 16:22:01 +0530

    Frontend integration v2.0.3

--- Distributed_Systems_Project/src/main/java/org/uom/distributed/systems/server/MonitoringServer.java
+++ Distributed_Systems_Project/src/main/java/org/uom/distributed/systems/server/MonitoringServer.java
@@ -3,6 +3,7 @@ package org.uom.distributed.systems.server;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
@@ -12,8 +13,27 @@ import org.uom.distributed.systems.NodeManager;
 
 public class MonitoringServer extends WebSocketServer {
     private static int client_count = 0;
+    private static AtomicBoolean systemIsRunning = new AtomicBoolean(false);
     private static final HashMap<String, WebSocket> webSocketHashMap = new HashMap<>(2);
     private NodeManager nodeManager;
+    Thread system = new Thread(() -> {
+        systemIsRunning.set(true);
+        nodeManager = new NodeManager(webSocketHashMap.get("COMMON"), webSocketHashMap.get("LOG"));
+
+        int[][] input = {
+                {2, 3, 345}, {12, 45, 234}, {35, 45, 533}, {1, 38, 234}, {4, 5, 20}, {50, 3, 98}, {22, 2, 144}, {34, 33, 233}, {11, 39, 235}, {4, 1, 4000}, {4, 2, 4000}, {4, 3, 4000}, {3, 4, 4000}, {2, 4, 4000}, {1, 4, 4000}
+        };
+//            int[][] input = {
+//                    {4, 1, 4000}, {4, 2, 4000}, {4, 3, 4000}, {3, 4, 4000}, {2, 4, 4000}, {1, 4, 4000}
+//            };
+
+        try {
+            nodeManager.initiateSystem(input);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    });
+
     public MonitoringServer(InetSocketAddress address) {
         super(address);
     }
@@ -23,34 +43,18 @@ public class MonitoringServer extends WebSocketServer {
     }
     @Override
     public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-        assert nodeManager != null; nodeManager.killSystem();webSocketHashMap.clear();
+        System.out.println("connection closed by the browser");
     }
 
     @Override
     public void onMessage(WebSocket conn, String message) {
         JSONObject json = new JSONObject(message);
         webSocketHashMap.put((String) json.get("SOCKET"), conn);
-        if(webSocketHashMap.containsKey("LOG") && webSocketHashMap.containsKey("COMMON")) {
-            if (client_count <= 2) {
-                nodeManager = new NodeManager(webSocketHashMap.get("COMMON"), webSocketHashMap.get("LOG"));
-
-                int[][] input = {
-                        {2, 3, 345}, {12, 45, 234}, {35, 45, 533}, {1, 38, 234}, {4, 5, 20}, {50, 3, 98}, {22, 2, 144}, {34, 33, 233}, {11, 39, 235}, {4, 1, 4000}, {4, 2, 4000}, {4, 3, 4000}, {3, 4, 4000}, {2, 4, 4000}, {1, 4, 4000}
-                };
-//            int[][] input = {
-//                    {4, 1, 4000}, {4, 2, 4000}, {4, 3, 4000}, {3, 4, 4000}, {2, 4, 4000}, {1, 4, 4000}
-//            };
-
-                try {
-                    nodeManager.initiateSystem(input);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            } else {
-                JSONObject e_message = new JSONObject().put("ERROR", "client already connected. Please restart the server and run the frontend.");
-                conn.send(e_message.toString());
-                conn.close();
-            }
+        if (webSocketHashMap.containsKey("LOG") && webSocketHashMap.containsKey("COMMON") && !systemIsRunning.get()) {
+            system.start();
+        } else if(systemIsRunning.get()){
+            System.out.println("hi");
+            conn.send(String.valueOf(new JSONObject().put("MESSAGE_TYPE","ERROR").put("MESSAGE","Old system is still running. please restart the server and try again.")));
         }
     }
 
