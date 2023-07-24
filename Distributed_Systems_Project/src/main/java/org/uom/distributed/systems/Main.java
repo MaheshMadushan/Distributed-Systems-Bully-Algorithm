12947da2632dc562e099402779b6de8ad7d69885
Mahesh <58337761+MaheshMadushan@users.noreply.github.com>
2023-07-24 14:51:04 +0530

    bug fixes v2.0.8

--- Distributed_Systems_Project/src/main/java/org/uom/distributed/systems/Main.java
+++ Distributed_Systems_Project/src/main/java/org/uom/distributed/systems/Main.java
@@ -108,23 +108,13 @@ public class Main {
     }
 
     private static boolean isNumber(char value) {
-        if (
-                value == '0' ||
-                value == '1' ||
-                value == '2' ||
-                value == '3' ||
-                value == '4' ||
-                value == '5' ||
-                value == '6' ||
-                value == '7' ||
-                value == '8' ||
-                value == '9'
-            ) {
+        if (value == '0' || value == '1' ||
+            value == '2' || value == '3' ||
+            value == '4' || value == '5' ||
+            value == '6' || value == '7' ||
+            value == '8' || value == '9') {
             return true;
         }
-
-
-
-        return false;
+                return false;
     }
 }
